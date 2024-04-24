78267600df66214d6156882be5159c8e72408fc2
Juan Angulo <juancarlosanguloabud@gmail.com>
2024-04-24 16:12:49 -0500

    Cambiado .json para mostrar preguntas incorrectas. Falta como el 30%

--- QuestionData.h
+++ QuestionData.h
@@ -5,324 +5,566 @@ std::string Data{ R"({
   "preguntas": [
     {
       "pregunta": "Cuando acabo la II Guerra Mundial?",
+      "incorrecta1": "1978",
+      "incorrecta2": "1979",
+      "incorrecta3": "1946",
       "respuesta": "1945"
     },
     {
       "pregunta": "Cual era el apellido de la reina Isabel II de Inglaterra?",
+      "incorrecta1": "Wulf",
+      "incorrecta2": "Volkswagen",
+      "incorrecta3": "Windsurf",
       "respuesta": "Windsor"
     },
     {
       "pregunta": "En que anio se aprobo la actual Constitucion espaniola?",
-      "respuesta": "En 1978"
+      "incorrecta1": "1975",
+      "incorrecta2": "1948",
+      "incorrecta3": "1979",
+      "respuesta": " 1978"
     },
     {
       "pregunta": "Que dia es la fiesta de la Hispanidad?",
-      "respuesta": "El 12 de octubre"
+      "incorrecta1": "10 de octubre",
+      "incorrecta2": "2 de octubre",
+      "incorrecta3": "18 de octubre",
+      "respuesta": "12 de octubre"
     },
     {
       "pregunta": "Cual fue el primer presidente democratico de Espania tras la dictadura franquista?",
+      "incorrecta1": "Adolf Hitler",
+      "incorrecta2": "Pepe Jimenez",
+      "incorrecta3": "El Pepe",
       "respuesta": "Adolfo Suarez"
     },
     {
       "pregunta": "En que anio murio Freddie Mercury?",
-      "respuesta": "En 1991"
+      "incorrecta1": "1993",
+      "incorrecta2": "1999",
+      "incorrecta3": "1992",
+      "respuesta": "1991"
     },
     {
       "pregunta": "Que dia es la fiesta nacional de Estados Unidos?",
+      "incorrecta1": "El 4 de junio",
+      "incorrecta2": "El 3 de julio",
+      "incorrecta3": "El 1 de julio",
       "respuesta": "El 4 de julio"
     },
     {
       "pregunta": "Quien invento la bombilla?",
+      "incorrecta1": "Abraham Lincoln",
+      "incorrecta2": "Nikola Tesla",
+      "incorrecta3": "Oppenheimer",
       "respuesta": "Thomas Edison"
     },
     {
       "pregunta": "En que pais se uso la primera bomba atomica?",
-      "respuesta": "En Japon Hiroshima"
+      "incorrecta1": "Estados Unidos",
+      "incorrecta2": "Alemania",
+      "incorrecta3": "Rusia",
+      "respuesta": "Japon Hiroshima"
     },
     {
       "pregunta": "Cuando empezo la Primera Guerra Mundial?",
-      "respuesta": "En 1914"
+      "incorrecta1": "1915",
+      "incorrecta2": "1924",
+      "incorrecta3": "1919",
+      "respuesta": "1914"
     },
     {
       "pregunta": "En que anio se produjo la Revolucion Francesa?",
-      "respuesta": "En 1789"
+      "incorrecta1": "1879",
+      "incorrecta2": "1780",
+      "incorrecta3": "1788",
+      "respuesta": "1789"
     },
     {
       "pregunta": "Cuantas veces ha estado el hombre en la Luna?",
-      "respuesta": "Seis"
+      "incorrecta1": "7",
+      "incorrecta2": "0",
+      "incorrecta3": "4",
+      "respuesta": "6"
     },
     {
       "pregunta": "Que presidente de Estados Unidos fue asesinado en Dallas?",
+      "incorrecta1": "Abraham Lincoln",
+      "incorrecta2": "Benjamin Franklin",
+      "incorrecta3": "Martin Luther King",
       "respuesta": "John F Kennedy"
     },
     {
       "pregunta": "En que anio viajo al espacio el primer ser humano?",
-      "respuesta": "En 1961 Yuri Gagarin"
+      "incorrecta1": "1962",
+      "incorrecta2": "1963",
+      "incorrecta3": "1960",
+      "respuesta": "1961"
     },
     {
       "pregunta": "En que anio llego Cristobal Colon a America?",
-      "respuesta": "En 1492"
+      "incorrecta1": "1493",
+      "incorrecta2": "1480",
+      "incorrecta3": "1495",
+      "respuesta": "1492"
     },
     {
       "pregunta": "Cuando salto Felix Baumgartner?",
-      "respuesta": "En 2012"
+      "incorrecta1": "2015",
+      "incorrecta2": "2011",
+      "incorrecta3": "2010",
+      "respuesta": "2012"
     },
     {
       "pregunta": "En que anio cayo el muro de Berlin?",
-      "respuesta": "En 1989"
+      "incorrecta1": "1980",
+      "incorrecta2": "1988",
+      "incorrecta3": "1987",
+      "respuesta": "1989"
     },
     {
       "pregunta": "Cuando se invento la imprenta?",
-      "respuesta": "En 1440"
+      "incorrecta1": "1430",
+      "incorrecta2": "1340",
+      "incorrecta3": "1540",
+      "respuesta": "1440"
     },
     {
       "pregunta": "Hace cuanto se extinguieron los dinosaurios?",
-      "respuesta": "Hace 66 millones de anios"
+      "incorrecta1": "20 MdA",
+      "incorrecta2": "54 MdA",
+      "incorrecta3": "39 MdA",
+      "respuesta": "66 MdA"
     },
     {
       "pregunta": "Cual era el nombre de pila de Che Guevara?",
+      "incorrecta1": "Pedro",
+      "incorrecta2": "Juan",
+      "incorrecta3": "Cesar",
       "respuesta": "Ernesto"
     },
     {
       "pregunta": "Segun la Biblia,cuantos anios vivio Matusalen?",
-      "respuesta": "969 anios"
+      "incorrecta1": "1000",
+      "incorrecta2": "862",
+      "incorrecta3": "789",
+      "respuesta": "969"
     },
     {
       "pregunta": "En que pais nacio Adolf Hitler?",
+      "incorrecta1": "Australia",
+      "incorrecta2": "Alemania",
+      "incorrecta3": "Holanda",
       "respuesta": "Austria"
     },
     {
       "pregunta": "Que reina britanica fue hija de los Reyes Catolicos?",
+      "incorrecta1": "Catrina de Aragon",
+      "incorrecta2": "Catalina de Alabama",
+      "incorrecta3": "Isabel de Astorias",
       "respuesta": "Catalina de Aragon"
     },
     {
       "pregunta": "Cuanto duro 'La Guerra de los Cien anios'?",
-      "respuesta": "116 anios"
+      "incorrecta1": "100",
+      "incorrecta2": "101",
+      "incorrecta3": "99",
+      "respuesta": "116"
     },
     {
       "pregunta": "Quien fue el ultimo faraon de Egipto?",
+      "incorrecta1": "Cleopatra",
+      "incorrecta2": "Egemones",
+      "incorrecta3": "Plato",
       "respuesta": "Ramses III"
     },
     {
       "pregunta": "Cual fue la primera civilizacion humana?",
+      "incorrecta1": "Imperio Chino",
+      "incorrecta2": "Incas",
+      "incorrecta3": "Mezopotania",
       "respuesta": "La civilizacion sumeria"
     },
     {
       "pregunta": "Cuando empezo la Revolucion Rusa?",
-      "respuesta": "En 1917"
+      "incorrecta1": "1920",
+      "incorrecta2": "1915",
+      "incorrecta3": "1919",
+      "respuesta": "1917"
     },
     {
       "pregunta": "Cual fue el genocidio mas grande de la historia?",
+      "incorrecta1": "El Retumbar",
+      "incorrecta2": "La vendimia",
+      "incorrecta3": "La venida del Senior",
       "respuesta": "El Holocausto"
     },
     {
       "pregunta": "Quien creo el Partido Comunista?",
+      "incorrecta1": "Marx",
+      "incorrecta2": "Chavez",
+      "incorrecta3": "Castillo",
       "respuesta": "Lenin"
     },
     {
       "pregunta": "Que famoso lider de Cuba estuvo 49 anios en el poder?",
+      "incorrecta1": "Hugo Chavez",
+      "incorrecta2": "Karl Marx",
+      "incorrecta3": "El Che Guevara",
       "respuesta": "Fidel Castro"
     },
     {
       "pregunta": "Cuando se fundo la OTAN?",
-      "respuesta": "En 1949"
+      "incorrecta1": "1940",
+      "incorrecta2": "1945",
+      "incorrecta3": "1950",
+      "respuesta": "1949"
     },
     {
       "pregunta": "En que anio fue asesinado Abraham Lincoln?",
-      "respuesta": "En 1865"
+      "incorrecta1": "1985",
+      "incorrecta2": "1860",
+      "incorrecta3": "1862",
+      "respuesta": "1865"
     },
     {
       "pregunta": "En que anio se hundio el Titanic?",
-      "respuesta": "En 1912"
+      "incorrecta1": "1915",
+      "incorrecta2": "1900",
+      "incorrecta3": "1899",
+      "respuesta": "1912"
     },
     {
       "pregunta": "Con cuantos anios murio la Reina Isabel II?",
-      "respuesta": "Con 96 anios"
+      "incorrecta1": "69",
+      "incorrecta2": "89",
+      "incorrecta3": "100",
+      "respuesta": "96"
     },
     {
       "pregunta": "Como se llamaba en realidad El Cid Campeador?",
+      "incorrecta1": "Este es Sech",
+      "incorrecta2": "El Pepe",
+      "incorrecta3": "Simon Bolivar",
       "respuesta": "Rodrigo Diaz de Vivar"
     },
     {
       "pregunta": "En que anio fue el golpe de estado de Tejero?",
-      "respuesta": "En 1981"
+      "incorrecta1": "1789",
+      "incorrecta2": "1680",
+      "incorrecta3": "1889",
+      "respuesta": "1981"
     },
     {
       "pregunta": "Cual fue el primer metal que el hombre empleo?",
-      "respuesta": "El cobre"
+      "incorrecta1": "Platino",
+      "incorrecta2": "Oro",
+      "incorrecta3": "Adamantium",
+      "respuesta": "cobre"
     },
     {
       "pregunta": "Que personaje fue conocido como el rey de Macedonia?",
+      "incorrecta1": "Poncio Pilatos",
+      "incorrecta2": "Arturo",
+      "incorrecta3": "Coraline",
       "respuesta": "Alejandro Magno"
     },
     {
       "pregunta": "Alrededor de que mar vivieron los egipcios, los fenicios, los israelitas, los griegos y los romanos?",
-      "respuesta": "El Mar Mediterraneo"
+      "incorrecta1": "Atlantico",
+      "incorrecta2": "Pacifico",
+      "incorrecta3": "Caribe",
+      "respuesta": "Mediterraneo"
     },
     {
       "pregunta": "quienes fundaron a Roma?",
+      "incorrecta1": "Ramo y Remulo",
+      "incorrecta2": "Nemo y Namo",
+      "incorrecta3": "Ramalo y Remulo",
       "respuesta": "Romulo y Remo"
     },
     {
       "pregunta": "En que anio ocurrio el intento de golpe de Estado en Espania?",
-      "respuesta": "En 1981"
+      "incorrecta1": "1982",
+      "incorrecta2": "1978",
+      "incorrecta3": "1988",
+      "respuesta": "1981"
     },
     {
       "pregunta": "En que anio se corono a Isabel II?",
+      "incorrecta1": "1978",
+      "incorrecta2": "1965",
+      "incorrecta3": "1923",
       "respuesta": "1953"
     },
     {
       "pregunta": "En que anio abdico el rey Juan Carlos I?",
-      "respuesta": "En 2014"
+      "incorrecta1": "2012",
+      "incorrecta2": "2015",
+      "incorrecta3": "2022",
+      "respuesta": "2014"
     },
     {
       "pregunta": "En que continente surgio la Humanidad?",
-      "respuesta": "En africa"
+      "incorrecta1": "Europa",
+      "incorrecta2": "Asia",
+      "incorrecta3": "America",
+      "respuesta": "Africa"
     },
     {
       "pregunta": "En que anio cayo el Imperio Romano?",
-      "respuesta": "En el anio 476"
+      "incorrecta1": "440",
+      "incorrecta2": "425",
+      "incorrecta3": "420",
+      "respuesta": "476"
     },
     {
       "pregunta": "En que etapa desaparecieron los dinosaurios?",
-      "respuesta": "Al final del Cretacico"
+      "incorrecta1": "Comienzo del Jurasico",
+      "incorrecta2": "Mediados del Jurasico",
+      "incorrecta3": "Comienzo del Cretacico",
+      "respuesta": "Final del Cretacico"
     },
     {
       "pregunta": "Quien descubrio Peru?",
+      "incorrecta1": "Simon Bolivar",
+      "incorrecta2": "Francisco de Miranda",
+      "incorrecta3": "Carlos Andres Perez",
       "respuesta": "Francisco Pizarro"
     },
     {
       "pregunta": "Donde nacio Adolf Hitler?",
-      "respuesta": "En Austria"
+      "incorrecta1": "Australia",
+      "incorrecta2": "Alemania",
+      "incorrecta3": "Holanda",
+      "respuesta": "Austria"
     },
     {
       "pregunta": "Quien fue el primer emperador romano?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Cesar Augusto"
     },
     {
       "pregunta": "En que anio murio Napoleon?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "En 1821"
     },
     {
       "pregunta": "Cual fue la ultima dinastia en China?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "La dinastia Qing"
     },
     {
-      "pregunta": "En que anio fue asesinado John F}, Kennedy?",
+      "pregunta": "En que anio fue asesinado John F Kennedy?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "En 1963"
     },
     {
       "pregunta": "En que pais se origino la civilizacion azteca?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "En Mexico"
     },
     {
       "pregunta": "Donde nacio Juana de Arco?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "En Francia"
     },
     {
       "pregunta": "Que paises fueron los miembros de los Aliados en la Segunda Guerra Mundial?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Gran Bretania, Francia, Rusia, China y Estados Unidos"
     },
     {
       "pregunta": "quien fue oficialmente el lider de la Union Sovietica?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Stalin"
     },
     {
       "pregunta": "Quien fue la mujer mas poderosa de la historia bizantina?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "La emperatriz Teodora"
     },
     {
       "pregunta": "Cuales fueron los cuatro grandes inventos de China?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "El papel, la brujula, la polvora y la imprenta"
     },
     {
       "pregunta": "Que pais de Asia es miembro del Eje en la Segunda Guerra Mundial?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Japon"
     },
     {
       "pregunta": "Quien fundo el socialismo cientifico?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Karl Marx"
     },
     {
       "pregunta": "Como se llaman los pobladores mas antiguos de Espania y Europa?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Homo anteccesor"
     },
     {
       "pregunta": "cual fue la otra que sobrevivio hasta nuestros dias?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Euskera"
     },
     {
       "pregunta": "en que anio el Califato de Damasco tomo Toledo?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "En el 711"
     },
     {
       "pregunta": "Que monarca convoco una Curia Regia extraordinaria que condujo al nacimiento de las Cortes?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Alfonso IX de Leon"
     },
     {
       "pregunta": "Como se llamaba el emir musulman que entrego las llaves de Granada a los Reyes Catolicos?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Boabdil"
     },
     {
       "pregunta": "A que rey se le atribuye 'el imperio en el que nunca se pone el sol'?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Carlos I"
     },
     {
       "pregunta": "Que rey envio a la Armada Invencible contra Inglaterra y perdio?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Felipe II"
     },
     {
       "pregunta": "Que monarca convoco una Curia Regia extraordinaria que condujo al nacimiento de las Cortes?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Alfonso IX de Leon"
     },
     {
       "pregunta": "Quien fue la primera mujer de Carlos II 'el Hechizado' ?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Maria Luisa de Orleans"
     },
     {
       "pregunta": "Bajo el reinado de quien se creo la Guardia Civil?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Isabel II"
     },
     {
       "pregunta": "Quien fue el segundo presidente de la II Republica?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "Manuel Azania"
     },
     {
       "pregunta": "Que dia encabezo Franco el alzamiento militar que inicio la guerra civil?",
+      "incorrecta1": "",
+      "incorrecta2": "",
+      "incorrecta3": "",
       "respuesta": "17 de julio"
     },
     {
       "pregunta": "En que anio se celebraron las primeras elecciones bajo la nueva constitucion?",
+      "incorrecta1": "1975",
+      "incorrecta2": "1989",
+      "incorrecta3": "1974",
       "respuesta": "1979"
     },
     {
       "pregunta": "Cuantos presidentes ha tenido la democracia espaniola en total hasta Pedro Sanchez?",
+      "incorrecta1": "9",
+      "incorrecta2": "8",
+      "incorrecta3": "6",
       "respuesta": "7"
     },
     {
       "pregunta": "Con que nombre se conoce el escandalo que obligo al presidente estadounidense Richard Nixon a dimitir?",
+      "incorrecta1": "Waterloo",
+      "incorrecta2": "Waterlop",
+      "incorrecta3": "Pizzagate",
       "respuesta": "Watergate"
     },
     {
       "pregunta": "Que emperador romano legalizo el cristianismo?",
+      "incorrecta1": "Pilatos",
+      "incorrecta2": "Plato",
+      "incorrecta3": "Socrates",
       "respuesta": "Constantino"
     },
     {
       "pregunta": "Que hito informatico de 1969 cambiaria radicalmente el curso de la historia de la humanidad?",
+      "incorrecta1": "Email",
+      "incorrecta2": "WhatsApp",
+      "incorrecta3": "Facebook",
       "respuesta": "Internet"
     },
     {
       "pregunta": "Quien fue el primer Presidente de Estados Unidos?",
+      "incorrecta1": "Tomas Edison",
+      "incorrecta2": "John F Kennedy",
+      "incorrecta3": "Abraham Lincoln",
       "respuesta": "George Washington"
     },
     {
       "pregunta": "Cual es el nombre de la famosa batalla donde Napoleon Bonaparte fue derrotado?",
+      "incorrecta1": "Earthwarm",
+      "incorrecta2": "Fireloop",
+      "incorrecta3": "Waterflop",
       "respuesta": "Waterloo"
     },
     {
       "pregunta": "Que faccion dirigio Mao Zedong durante la guerra civil China?",
+      "incorrecta1": "Capitalista",
+      "incorrecta2": "Centralista",
+      "incorrecta3": "Nula",
       "respuesta": "Comunistas"
     }
   ]
 }
+
+
 )" };
